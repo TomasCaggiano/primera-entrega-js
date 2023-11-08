c1b3f604d05027a21810496e80b2006c8269b965
Tomas Caggiano <tomascaggiano03@gmail.com>
2023-11-08 12:44:02 -0300

    reentrega-primeraentrega

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,26 @@
 let seguir;
 
-do{let componente = prompt("seleccione el componente que desea adquirir:(gpu, mother,gabinete,hdd,ssd,ram,fuente)");
-
-let cantidad =prompt("cuantos quiere?");{
-    if(componente === "gpu") {console.log("valor total:" + 50 * cantidad)}
-else if(componente === "mother"){console.log("valor total:" + 30 * cantidad)}
-else if(componente === "gabinete"){console.log("valor total:" + 25 * cantidad)}
-else if(componente === "hdd"){console.log("valor total:" + 10 * cantidad)}
-else if(componente === "ssd"){console.log("valor total:" + 15 * cantidad)}
-else if(componente === "ram"){console.log("valor total:" + 5 * cantidad)}
-else if(componente === "fuente"){console.log("valor total:" + 20 * cantidad) }
-else{console.log("componente no encontrado")
+do{let componente = prompt("seleccione el componente que desea adquirir:(gpu, mother,gabinete,hdd,ssd,ram,fuente)").toLowerCase();
+if(componente === "gpu" || "mother" || "gabinete" || "hdd" || "ssd" || "ram" || "fuente"){ 
+let cantidad =parseFloat(prompt("cuantos quiere?"));{
+    if(componente === "gpu") {alert("valor total:" + 50 * cantidad)}
+else if(componente === "mother"){alert("valor total:" + 30 * cantidad)}
+else if(componente === "gabinete"){alert("valor total:" + 25 * cantidad)}
+else if(componente === "hdd"){alert("valor total:" + 10 * cantidad)}
+else if(componente === "ssd"){alert("valor total:" + 15 * cantidad)}
+else if(componente === "ram"){alert("valor total:" + 5 * cantidad)}
+else if(componente === "fuente"){alert("valor total:" + 20 * cantidad) }
+else{alert("componente no encontrado")
 }
 }let continuar = prompt("desea continuar comprando? si/no")
 if (continuar === "si"){seguir = true}else{seguir =false}
-}while(seguir)
+}}while(seguir)
 
+function total(){
+let totalcompra1 = parseFloat(prompt("introduzca el valor total de su primera compra: "));
+let totalcompra2 = parseFloat(prompt("introduzca el valor total de su segunda compra: "));
+let totalcompra = totalcompra1 + totalcompra2;
+alert("el total de su compra es " + totalcompra );
+}
 
-
+total()
